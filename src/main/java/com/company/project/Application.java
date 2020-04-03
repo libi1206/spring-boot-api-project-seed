8feb7e6cd026daf0db5bc57ae03c30a721ac7628
李比 <843298330@qq.com>
2020-04-03 10:14:56 +0800

    feat: 调整项目目录结构

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -1,9 +1,13 @@
 package com.company.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.company.project.core.constent.ProjectConstant.*;
+
 @SpringBootApplication
+@MapperScan(basePackages = {MAPPER_PACKAGE})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
